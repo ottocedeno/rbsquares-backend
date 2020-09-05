a1c99f2d58ce4dd094d8bf4e9637ad27cbfa2e49
ottocedeno <otto@creativespaceclub.com>
2020-09-04 23:44:27 -0400

    Added seed data for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+3.times do
+  User.create(
+    username: Faker::Movies::StarWars.character.downcase.gsub(/[\s-]/, ""),
+    password: 'dev'
+  )
+end
