8fdc213ad9833d34c7b1b496287393f1af1871d9
ottocedeno <otto@creativespaceclub.com>
2020-09-04 23:21:37 -0400

    Added default values for User instances

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  after_initialize :default_values
+
+  private 
+
+  def default_values
+    self.balance = 500
+    self.winstreak = 0
+  end
 end
