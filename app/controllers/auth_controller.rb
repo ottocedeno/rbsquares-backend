79adc7b2527e8ebb6495c1e520b22b3f8c60d42e
ottocedeno <otto@creativespaceclub.com>
2020-09-07 00:06:52 -0400

    Setup JWT methods

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,12 @@
 class AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   def create
     user = User.find_by(username: user_login_params[:username])
 
     if user && user.authenticate(user_login_params[:password])
-      render json: {user: UserSerializer.new(user)}, status: :ok
+      token = encode_token({user_id: user.id})
+      render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
     else
       render json: {message: "Sorry, your credentials are invalid"}, status: :unauthorized
     end
