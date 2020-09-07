e84162c845c31de80624f2e3063f575d31a1bf29
ottocedeno <otto@creativespaceclub.com>
2020-09-06 22:43:28 -0400

    Added Auth controller and initial setup for user login flow

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,17 @@
+class AuthController < ApplicationController
+
+  def create
+    @user = User.find_by(username: user_login_params[:username])
+
+    if @user && @user.authenticate(user_login_params[:password])
+      render json: {user: @user}, status: :ok
+    else
+      render json: {message: "Sorry, your credentials are invalid"}, status: :unauthorized
+    end
+    
+  end
+
+  def user_login_params
+    params.require(:user).permit(:username, :password)
+  end
+end
