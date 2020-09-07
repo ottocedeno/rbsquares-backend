475c0322c8dfbc5fc05290966f680494e7ead13d
ottocedeno <otto@creativespaceclub.com>
2020-09-06 23:29:57 -0400

    Created initial User Serializer

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,10 @@
 class AuthController < ApplicationController
 
   def create
-    @user = User.find_by(username: user_login_params[:username])
+    user = User.find_by(username: user_login_params[:username])
 
-    if @user && @user.authenticate(user_login_params[:password])
-      render json: {user: @user}, status: :ok
+    if user && user.authenticate(user_login_params[:password])
+      render json: {user: UserSerializer.new(user)}, status: :ok
     else
       render json: {message: "Sorry, your credentials are invalid"}, status: :unauthorized
     end
