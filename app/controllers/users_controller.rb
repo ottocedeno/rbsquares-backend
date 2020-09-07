ef981f6a4dc927ab57e9031d9aa7737f3eb28e30
ottocedeno <otto@creativespaceclub.com>
2020-09-07 18:44:36 -0400

    User Create Controllor action set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def create
+    user = User.new(user_params)
+
+    if user.valid?
+      token = encode_token({user_id: user.id})
+      render json: {user: UserSerializer.new(user), jwt: token}, status: :created
+    else
+      render json: {message: 'Cannot create that username, try again.'}, status: :not_acceptable
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
