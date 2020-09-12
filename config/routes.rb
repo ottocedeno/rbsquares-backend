b443386f959fa450901a54bd9cd2697498d18e79
ottocedeno <otto@creativespaceclub.com>
2020-09-12 14:49:04 -0400

    updated /retrieve route to point to Auth Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
-  get '/retrieve' => 'users#retrieve'
+  get '/retrieve' => 'auth#retrieve'
   post '/login' => 'auth#create'
 end
