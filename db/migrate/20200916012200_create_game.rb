7230ac9733ff23e2f7c12fe368578602d65622f3
ottocedeno <otto@creativespaceclub.com>
2020-09-15 21:35:47 -0400

    Created Game DB table

---
+++ db/migrate/20200916012200_create_game.rb
@@ -0,0 +1,13 @@
+class CreateGame < ActiveRecord::Migration[6.0]
+  def change
+    create_table :game do |t|
+      t.integer :user_id
+      t.integer :bet_amount
+      t.boolean :matching_square?
+      t.boolean :matching_color?
+      t.integer :payout
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_025005) do
+ActiveRecord::Schema.define(version: 2020_09_16_012200) do
+
+  create_table "game", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "bet_amount"
+    t.boolean "matching_square?"
+    t.boolean "matching_color?"
+    t.integer "payout"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
