7ab4e7949f52ed96e9acf6e89abef54ac0efbfb9
ottocedeno <otto@creativespaceclub.com>
2020-09-16 21:55:14 -0400

    added Game model validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,10 @@
 class Game < ApplicationRecord
   belongs_to :user
-  before_save update_user_balance
+  before_save :update_user_balance
+  validates :bet_amount, presence: true
+  validates :matching_square?, presence: true
+  validates :matching_color?, presence: true
+  validates :payout, presence: true
 
   def update_user_balance
     user.balance += payout if payout > 0
