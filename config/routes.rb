49e439af260d77a094d78c628d87d3d3864ea1e0
ottocedeno <otto@creativespaceclub.com>
2020-09-21 21:27:03 -0400

    added index route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create]
+  resources :users, only: [:create, :index]
   resources :games, only: [:create]
   get '/retrieve' => 'auth#retrieve'
   post '/login' => 'auth#create'
